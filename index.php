f6359ac8627bbb3b35e43fa9c9cc94a5c7f62955
Hien Pham <hien.pham@isobar.com>
2020-08-17 13:54:34 +0700

    test

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ $post = new \Post\Model\Post();
 $p = 1;
 $posts = $post->getList('', 10, $p, 'DESC');
 $postData = reset($posts);
-$time = strtotime($postData->updated_at);
-$postData->updated_at = date('F j, Y',$time);
+$time = date('F j, Y | H:i',strtotime($postData->updated_at));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +13,7 @@ $postData->updated_at = date('F j, Y',$time);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>Vietnam Weather</title>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" />
-	<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
+    <link rel="stylesheet" href="css/styles.css"/>
 </head>
 <body>
 <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
@@ -24,10 +23,29 @@ $postData->updated_at = date('F j, Y',$time);
         <div class="inner">
 			<!--Đổi dòng tiêu đề to của page-->
             <h3 class="masthead-brand">Vietnam Weather <br/>for Amateur Astronomy Observatory</h3>
-            <nav class="nav nav-masthead justify-content-center">
-                <a class="nav-link active" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Contact</a>
+            <nav class="nav-masthead navbar navbar-expand-lg navbar-dark">
+                <div class="collapse navbar-collapse" id="main_nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"> <a class="nav-link active" href="#">Home</a> </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">  Stations </a>
+                            <ul class="dropdown-menu bg-dark">
+                                <li><a class="dropdown-item text-white" href="#"> AllSky</a></li>
+                                <li><a class="dropdown-item text-white" href="#"> BloomSky </a></li>
+								<li><a class="dropdown-item text-white" href="#"> Ambient </a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"> Lab </a>
+                            <ul class="dropdown-menu bg-dark">
+                                <li><a class="dropdown-item text-white" href="#"> Apps </a></li>
+                                <li><a class="dropdown-item text-white" href="#"> Books </a></li>
+								<li><a class="dropdown-item text-white" href="#"> Astro Basic</a></li>
+								<li><a class="dropdown-item text-white" href="#"> Weather Basic</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div> <!-- navbar-collapse.// -->
             </nav>
             <div class="masthead-brand">
                 <!--Chỉnh sửa câu chú thích Page-->
@@ -43,34 +61,44 @@ $postData->updated_at = date('F j, Y',$time);
                     <div class="col-md-12" style="background: #333;">
                         <div class="card flex-md-row mb-4 box-shadow h-md-250 text-white" style="background: #333;">
                             <div class="card-body d-flex flex-column align-items-start w-50">
-                                <strong class="d-inline-block mb-2 text-primary">Hot News</strong>
-                                <h3 class="mb-0">
-                                    <a class="text-white" href="#"><?= $postData->title ?></a>
-                                </h3>
-                                <div class="mb-1 text-muted"><?= $postData->updated_at ?></div>
-                                <p class="card-text mb-auto"><?= $postData->short_description ?></p>
-                                <p class="card-text mb-auto"><?= $postData->content ?></p>
-                                <a href="#">Continue reading</a>
+                                <strong class="d-inline-block mb-2 text-primary">Update News</strong>
+                                <div class="mb-1 text-muted"><?= $time ?></div>
+								<h3 class="mb-0">
+                                    <a href="./detail.php?id=<?= $postData->post_id ?>"><?= $postData->title ?></a>
+                                </h3>                                
+                                <p class="card-text"><?= $postData->short_description ?></p>
+                                <p class="card-text"><?= mb_strimwidth($postData->content, 0, 300, "..."); ?></p>
+                                <!--<a href="#">Continue reading</a>-->
                             </div>
-                            <div class="p-4 w-50">
-                                <img class="w-100 card-img-right flex-auto d-none d-md-block" data-src="./img/<?=$postData->image ?>" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="./img/<?=$postData->image ?>" data-holder-rendered="true">
+                            <div class="p-4 w-50 d-flex">
+                                <div class="row align-self-center w-100">
+                                    <img class="w-100" data-src="./img/<?=$postData->image ?>" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="./img/<?=$postData->image ?>" data-holder-rendered="true">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            <div class="text-center w-100">
+                <div class="fb-share-button"
+                     data-href="./img/<?=$postData->image ?>"
+                     data-layout="button">
+                </div>
+            </div>
             <?php endif;?>
         </div>
 <!--Code chia thành 2 hình lớn width=100-->
         <div class="d-flex flex-row flex-wrap mt-2 mb-2">
             <div class="p-4 w-50">
-                <img class="w-100" src="https://api.sat24.com/animated/TH/visual/3/SE%20Asia%20Standard%20Time/7560706" alt="">
-                <div class="fb-share-button"
+				<img style="margin-bottom:1.5em;" class="w-100" src="https://api.sat24.com/animated/TH/visual/3/SE%20Asia%20Standard%20Time/7560706" alt="">
+				<br/>
+				<div class="fb-share-button"
                      data-href="https://api.sat24.com/animated/TH/visual/3/SE%20Asia%20Standard%20Time/7560706"
                      data-layout="button">
-                </div>
+				</div>
             </div>
             <div class="p-4 w-50">
-                <img class="w-100" src="https://api.sat24.com/animated/TH/infraPolair/3/SE%20Asia%20Standard%20Time/7417866" alt="">
+				<img style="margin-bottom:1.5em;"class="w-100" src="https://api.sat24.com/animated/TH/infraPolair/3/SE%20Asia%20Standard%20Time/7417866" alt="">
+				<br/>
                 <div class="fb-share-button"
                      data-href="https://api.sat24.com/animated/TH/infraPolair/3/SE%20Asia%20Standard%20Time/7417866"
                      data-layout="button">
@@ -89,6 +117,7 @@ $postData->updated_at = date('F j, Y',$time);
                 <iframe class="embed-responsive-item" src="https://embed.windy.com/embed2.html?lat=16.215&lon=106.743&detailLat=17.393&detailLon=106.479&width=650&height=650&zoom=5&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=%C2%B0C&radarRange=-1" frameborder="0">
 				</iframe>
             </div>
+			
             <div class="p-4">
                 <div class="fb-share-button"
                      data-href="https://embed.windy.com/embed2.html?lat=16.215&lon=106.743&detailLat=17.393&detailLon=106.479&width=650&height=650&zoom=5&level=surface&overlay=wind&product=ecmwf&menu=&message=&marker=&calendar=now&pressure=true&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=%C2%B0C&radarRange=-1"
@@ -105,22 +134,22 @@ $postData->updated_at = date('F j, Y',$time);
         </div>
         <div class="d-flex flex-row mt-2 mb-2 flex-wrap">
             <div class="p-4 col-sm-6">
-                <P>Realtime BloomSky Image</P>
+                <P>Realtime BloomSky Image: Go Vap, Saigon</P>
                 <img class="w-100 h-100" src="./img/bloomsky.jpg" type="video/mp4"alt="real time bllomsky">
             </div>
             <div class="p-4 col-sm-6" >
-                <P>Realtime BloomSky Time-lapse</P>
+                <P>Realtime BloomSky Time-lapse: Go Vap, Saigon</P>
                 <video class="w-100 h-100 border border-white" src="./video/bloomsky.mp4" type="video/mp4" alt="" controls></video>
             </div>
             <div class="p-4 col-sm-6">
-                <div class="small">Update every 10 minutes</div>
+                <div class="small">Update every hour</div>
                 <div class="fb-share-button"
                      data-href="./img/bloomsky.jpg"
                      data-layout="button">
                 </div>
             </div>
             <div class="p-4 col-sm-6">
-                <div class="small">Update every 10 minutes</div>
+                <div class="small">Update every day</div>
                 <div class="fb-share-button"
                      data-href="./video/bloomsky.mp4"
                      data-layout="button">
@@ -138,7 +167,7 @@ $postData->updated_at = date('F j, Y',$time);
 <!--            <iframe src="http://78.23.108.227:10800" width="800" height="1554" scrolling="NO" img="" border="0"> </iframe>-->
 <!--        </div>-->
         
-        <div class="cover mt-3 mb-3"></div>
+        <!-- An di AAG<div class="cover mt-3 mb-3"></div>		
         <div class="mb-2">
             <p class="mt-2">Magnitude of the sky background in Magn./Arc.sec²</p>
             <div class="small">Update every 15 minutes</div>
@@ -166,7 +195,7 @@ $postData->updated_at = date('F j, Y',$time);
                 </div>
             </div>
         </div>
-        <div class="cover mt-3 mb-3"></div>
+        <div class="cover mt-3 mb-3"></div>-->
         <div class="mb-5">
             <p class="mt-5 mb-5">Seeing conditions Saigon, Vietnam</p>
 			
@@ -181,12 +210,18 @@ $postData->updated_at = date('F j, Y',$time);
 		
 		
         <div class="mt-5 d-flex flex-row text-left contact p-2">
-            <div class="w-50">
-                <p>Lastest News</p>
+            <div class="w-50" style="margin-right:2em;">
+                <p style="color:white"><b>Lastest News - <a href="http://weathervn.com/list.php" style="font-size:12px;">Click here for older news</a></b></p>
                 <div class="cover"></div>
                 <ul class="text-white">
                    <?php foreach ($posts as $post):?>
-                       <li class=""><?= $post->title ?></li>
+                       <?php
+                       $time = strtotime($post->updated_at);
+                       $post->updated_at = date('F j, Y | H:i',$time);
+                       ?>
+                       <li class="">
+                           <a href="./detail.php?id=<?= $post->post_id ?>"><?= $post->updated_at ?> <br> <?= $post->title ?></a>
+                       </li>
                     <?php endforeach;?>
                 </ul>
             </div>
@@ -228,7 +263,6 @@ $postData->updated_at = date('F j, Y',$time);
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v7.0&appId=163517574259703&autoLogAppEvents=1" nonce="IJeVUNEQ"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
-<script src="//cdn.jsdelivr.net/npm/less" ></script>
 <script>
 $( document ).ready(function() {
 	$('#submitForm').click(function(){
@@ -251,16 +285,16 @@ $( document ).ready(function() {
 			$('#contactForm').trigger("reset");
 		});
 	});
-	$('img').click(function(){
-        fbShare(this.src, 'Fb Share', 'Facebook share popup', this.src, 520, 350);
-    });
+	// $('img').click(function(){
+    //     fbShare(this.src, 'Fb Share', 'Facebook share popup', this.src, 520, 350);
+    // });
 });
 
-function fbShare(url, title, descr, image, winWidth, winHeight) {
-    var winTop = (screen.height / 2) - (winHeight / 2);
-    var winLeft = (screen.width / 2) - (winWidth / 2);
-    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-}
+// function fbShare(url, title, descr, image, winWidth, winHeight) {
+//     var winTop = (screen.height / 2) - (winHeight / 2);
+//     var winLeft = (screen.width / 2) - (winWidth / 2);
+//     window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+// }
 </script>
 </body>
 </html>
